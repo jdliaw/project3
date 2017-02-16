b0728e521fb1ebba2f770fe54c95762919598744
jdliaw <jennifer.liaw07@gmail.com>
2017-02-15 22:41:21 -0800

    DbManager parses everything from database

--- src/edu/ucla/cs/cs144/DbManager.java
+++ src/edu/ucla/cs/cs144/DbManager.java
@@ -1,5 +1,7 @@
 package edu.ucla.cs.cs144;
 
+import java.sql.Statement;
+import java.sql.ResultSet;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -9,7 +11,12 @@ public class DbManager {
         static private String dbname = "CS144";
         static private String username = "cs144";
         static private String password = "";
-	
+
+        static private String item_name, description;
+        static private int item_id;
+        static private String category;
+        static private int category_itemId;
+
 	/**
 	 * Opens a database connection
 	 * @param dbName The database name
@@ -18,18 +25,44 @@ public class DbManager {
 	 * @throws SQLException
 	 */
 	public static Connection getConnection(boolean readOnly)
-	throws SQLException {        
+	throws SQLException {
             Connection conn = DriverManager.getConnection(
                 databaseURL + dbname, username, password);
-            conn.setReadOnly(readOnly);        
+            conn.setReadOnly(readOnly);
             return conn;
         }
-	
+
 	private DbManager() {}
-	
+
 	static {
 		try {
 			Class.forName("com.mysql.jdbc.Driver").newInstance();
+
+      Connection c = getConnection(true);
+
+      Statement s = c.createStatement();
+
+      // Execute select statement for Items
+      ResultSet items_rs = s.executeQuery("SELECT * FROM Item");
+      while (items_rs.next()) {
+        item_name = items_rs.getString("Name");
+        description = items_rs.getString("Description");
+        item_id = items_rs.getInt("ItemId");
+        // System.out.println("Item " + item_id + ": " + item_name + " (" + description + ")");
+      }
+
+      // Execute select statement for Category
+      ResultSet cat_rs = s.executeQuery("SELECT * FROM Category");
+      while (cat_rs.next()) {
+        category = cat_rs.getString("Category");
+        category_itemId = cat_rs.getInt("ItemId");
+        // System.out.println("Category " + category + " - " + category_itemId);
+      }
+
+      items_rs.close();
+      s.close();
+      c.close();
+
 		} catch(Exception e) {
 			e.printStackTrace();
 			throw new RuntimeException(e);
