e0a74db5a1f8473dcee649355112063eecd462c6
jdliaw <jennifer.liaw07@gmail.com>
2017-02-16 15:31:40 -0800

    Push Indexer changes

--- src/edu/ucla/cs/cs144/Indexer.java
+++ src/edu/ucla/cs/cs144/Indexer.java
@@ -23,53 +23,106 @@ import org.apache.lucene.store.FSDirectory;
 import org.apache.lucene.util.Version;
 
 public class Indexer {
-    
+
     /** Creates a new instance of Indexer */
     public Indexer() {
     }
- 
+
+    private IndexWriter indexWriter = null;
+
+    public IndexWriter getIndexWriter(boolean create) throws IOException {
+        if (indexWriter == null) {
+            Directory indexDir = FSDirectory.open(new File("/var/lib/lucene/"));
+            IndexWriterConfig config = new IndexWriterConfig(Version.LUCENE_4_10_2, new StandardAnalyzer());
+            indexWriter = new IndexWriter(indexDir, config);
+        }
+        return indexWriter;
+    }
+
+    public void closeIndexWriter() throws IOException {
+        if (indexWriter != null) {
+            indexWriter.close();
+        }
+    }
+
+    public void indexItem(Item item) throws IOException {
+        System.out.println("Indexing item: " + item);
+        IndexWriter writer = getIndexWriter(false);
+        Document doc = new Document();
+        doc.add(new StringField("id", item.getId(), Field.Store.YES));
+        doc.add(new StringField("name", item.getName(), Field.Store.YES));
+        doc.add(new TextField("description", item.getDescription(), Field.Store.YES));
+        String fullSearchableText = item.getName() + " " + item.getDescription();
+        doc.add(new TextField("content", fullSearchableText, Field.Store.NO));
+        writer.addDocument(doc);
+    }
+
+    public void indexCategory(Category category) throws IOException {
+        System.out.println("Indexing category: " + category);
+        IndexWriter writer = getIndexWriter(false);
+        Document doc = new Document();
+        doc.add(new StringField("itemId", category.getItemId(), Field.Store.YES));
+        doc.add(new StringField("name", category.getName(), Field.Store.YES));
+        String fullSearchableText = category.getName();
+        doc.add(new TextField("content", fullSearchableText, Field.Store.NO));
+        writer.addDocument(doc);
+    }
+
     public void rebuildIndexes() {
 
         Connection conn = null;
 
         // create a connection to the database to retrieve Items from MySQL
-	try {
-	    conn = DbManager.getConnection(true);
-	} catch (SQLException ex) {
-	    System.out.println(ex);
-	}
+        try {
+            conn = DbManager.getConnection(true);
+        } catch (SQLException ex) {
+            System.out.println(ex);
+        }
 
 
-	/*
-	 * Add your code here to retrieve Items using the connection
-	 * and add corresponding entries to your Lucene inverted indexes.
+        /*
+         * Add your code here to retrieve Items using the connection`
+         * and add corresponding entries to your Lucene inverted indexes.
+             *
+             * You will have to use JDBC API to retrieve MySQL data from Java.
+             * Read our tutorial on JDBC if you do not know how to use JDBC.
+             *
+             * You will also have to use Lucene IndexWriter and Document
+             * classes to create an index and populate it with Items data.
+             * Read our tutorial on Lucene as well if you don't know how.
+             *
+             * As part of this development, you may want to add
+             * new methods and create additional Java classes.
+             * If you create new classes, make sure that
+             * the classes become part of "edu.ucla.cs.cs144" package
+             * and place your class source files at src/edu/ucla/cs/cs144/.
          *
-         * You will have to use JDBC API to retrieve MySQL data from Java.
-         * Read our tutorial on JDBC if you do not know how to use JDBC.
-         *
-         * You will also have to use Lucene IndexWriter and Document
-         * classes to create an index and populate it with Items data.
-         * Read our tutorial on Lucene as well if you don't know how.
-         *
-         * As part of this development, you may want to add 
-         * new methods and create additional Java classes. 
-         * If you create new classes, make sure that
-         * the classes become part of "edu.ucla.cs.cs144" package
-         * and place your class source files at src/edu/ucla/cs/cs144/.
-	 * 
-	 */
+         */
 
+        getIndexWriter(true);
+        // Index all item entries
+        Item[] items = DbManager.getItems(); // this is supposed to be done in JDBC
+        for (Item item : items) {
+            // System.out.println(item.getName());
+            indexItem(item);
+        }
+        // Index all category entries
+        Category[] categories = DbManager.getCategories();
+        for (Category category : categories) {
+            // System.out.println(category.getName());
+            indexCategory(category);
+        }
 
-        // close the database connection
-	try {
-	    conn.close();
-	} catch (SQLException ex) {
-	    System.out.println(ex);
-	}
-    }    
+            // close the database connection
+        try {
+            conn.close();
+        } catch (SQLException ex) {
+            System.out.println(ex);
+        }
+    }
 
     public static void main(String args[]) {
         Indexer idx = new Indexer();
         idx.rebuildIndexes();
-    }   
+    }
 }
