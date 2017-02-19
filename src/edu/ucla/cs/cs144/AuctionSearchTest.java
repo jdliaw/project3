e82355a7151f4084f558c21a826d80fb25329f5a
jdliaw <jennifer.liaw07@gmail.com>
2017-02-18 18:46:20 -0800

    search spell

--- src/edu/ucla/cs/cs144/AuctionSearchTest.java
+++ src/edu/ucla/cs/cs144/AuctionSearchTest.java
@@ -20,13 +20,13 @@ public class AuctionSearchTest {
 
 		String query = "superman";
 		SearchResult[] basicResults = as.basicSearch(query, 0, 4000);
-		System.out.println("Basic Seacrh Query: " + query);
+		System.out.println("Basic Search Query: " + query);
 		System.out.println("Received " + basicResults.length + " results");//\nShould be 68 matches\n");
 
 		query = "kitchenware";
 		basicResults = null;
 		basicResults = as.basicSearch(query, 0, 1500);
-		System.out.println("Basic Seacrh Query: " + query);
+		System.out.println("Basic Search Query: " + query);
 		System.out.println("Received " + basicResults.length + " results");//\nShould be 1462 matches\n");
 
 
@@ -34,7 +34,7 @@ public class AuctionSearchTest {
 		query = "star trek";
 		basicResults = null;
 		basicResults = as.basicSearch(query, 0, 3000);
-		System.out.println("Basic Seacrh Query: " + query);
+		System.out.println("Basic Search Query: " + query);
 		System.out.println("Received " + basicResults.length + " results\n");//\nShould be 770 matches\n");
 
 
@@ -49,7 +49,7 @@ public class AuctionSearchTest {
 		SearchRegion region =
 		    new SearchRegion(33.774, -118.63, 34.201, -117.38);
 		SearchResult[] spatialResults = as.spatialSearch("camera", region, 0, 20);
-		System.out.println("Spatial Seacrh");
+		System.out.println("Spatial Search");
 		System.out.println("Received " + spatialResults.length + " results");
 		for(SearchResult result : spatialResults) {
 			System.out.println(result.getItemId() + ": " + result.getName());
